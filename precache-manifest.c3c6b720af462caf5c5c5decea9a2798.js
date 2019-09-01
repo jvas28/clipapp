d4dfc73fb7ba12ccc9e2ade31bf18002e21532d0
Julio  Vásconez <jvasconez28@gmail.com>
2019-09-01 17:27:56 +0200

    Production build

---
+++ precache-manifest.c3c6b720af462caf5c5c5decea9a2798.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fa0297cf9922518bf16d1d3e816c887",
+    "url": "/clipapp/index.html"
+  },
+  {
+    "revision": "186c7178e47ebc294cd9",
+    "url": "/clipapp/static/css/2.b3646333.chunk.css"
+  },
+  {
+    "revision": "f1bb40cab026582ddd90",
+    "url": "/clipapp/static/css/main.222a563d.chunk.css"
+  },
+  {
+    "revision": "186c7178e47ebc294cd9",
+    "url": "/clipapp/static/js/2.47dc3e88.chunk.js"
+  },
+  {
+    "revision": "f1bb40cab026582ddd90",
+    "url": "/clipapp/static/js/main.2a644cfa.chunk.js"
+  },
+  {
+    "revision": "6d127dc9afd00f9612eb",
+    "url": "/clipapp/static/js/runtime~main.06e4b604.js"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/clipapp/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/clipapp/static/media/icons.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/clipapp/static/media/icons.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/clipapp/static/media/icons.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/clipapp/static/media/icons.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/clipapp/static/media/icons.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.17dc5f9251dbe06b87e49d1b575deaaa.js"
+  "/clipapp/precache-manifest.c3c6b720af462caf5c5c5decea9a2798.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/clipapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
